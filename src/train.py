43a20c2965d295248ea8c95f6a57cd69f7a44960
vedantthapa <thapavedant@gmail.com>
2022-04-12 21:31:35 +0000

    Adds function documentation

--- src/train.py
+++ src/train.py
@@ -3,7 +3,22 @@ from pyspark.ml.feature import StringIndexer, VectorAssembler
 from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import RegressionEvaluator
 
+
 def train_model(data, regressor, labelCol, params={}):
+    """
+    Returns the trained pipeline and Spark DataFrame with relevant inputs and predictions
+
+    Parameters
+    ----------
+    data : Spark DataFrame
+        The input DataFrame
+    regressor : obj
+        The regression model object from MLlib
+    labelCol : str
+        The name of the target column in the DataFrame.
+    params : dict, optional
+        Parameters for the PySpark regression model as key-value pairs
+    """
     
     pollutant_mean = labelCol.split()[0] + ' Mean'
     inputCols = ['Year', 'Month', 'Day', 'County_index', 'City_index', pollutant_mean]
@@ -18,16 +33,13 @@ def train_model(data, regressor, labelCol, params={}):
     stages = indexers + [assembler, m]
     pipeline = Pipeline(stages=stages)
     
-    model = pipeline.fit(train)
-    
-    preds_tr = model.transform(train)
+    model = pipeline.fit(train)  
     preds_ts = model.transform(test)
     
     evaluator = RegressionEvaluator(labelCol=labelCol, predictionCol="prediction", metricName="rmse")
-    train_rmse = evaluator.evaluate(preds_tr)
     test_rmse = evaluator.evaluate(preds_ts)
    
     print(f"Root Mean Squared Error (RMSE) on test data = {test_rmse}")
     print()
     
-    return preds_ts[inputCols + ['Date', labelCol, 'features', 'prediction', 'City']]
+    return model, preds_ts[inputCols + ['Date', labelCol, 'features', 'prediction', 'City']]
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,19 @@
 import matplotlib.pyplot as plt
 
-def plot_splits(pollutant, train, test):    
+def plot_splits(pollutant, train, test):
+    """
+    Plot the time-series train and test splits
+
+    Parameters
+    ----------
+    pollutant : str
+        Name of the pollutant column
+    train : Spark DataFrame
+        The train dataframe
+    test : str
+        The test DataFrame
+    """
+    
     pd_train = train.groupby('Date').agg({pollutant: 'mean'}).orderBy('Date').toPandas().set_index('Date')
     pd_test = test.groupby('Date').agg({pollutant: 'mean'}).orderBy('Date').toPandas().set_index('Date')
     
@@ -13,6 +26,18 @@ def plot_splits(pollutant, train, test):
     
 
 def create_splits(data, target_col, plot=False):
+    """
+    Splits and returns the dataset into train and test
+
+    Parameters
+    ----------
+    data : Spark DataFrame
+        The input DataFrame
+    target_col : str
+        The name of the target column
+    plot : boolean, optional
+        If True, it calls the plot_split function to plot the dataset splits
+    """
     train = data[(data['Date'] > '2015-01-01') & (data['Date'] <= '2021-01-01')]
     test = data[data['Date'] > '2021-01-01']
     
@@ -24,6 +49,19 @@ def create_splits(data, target_col, plot=False):
 
 
 def plot_city_preds(pollutant, data, city):
+    """
+    Plots the predictions obtained by the model based on a city
+
+    Parameters
+    ----------
+    pollutant : str
+        Name of the pollutant column
+    data : Spark DataFrame
+        The input DataFrame
+    city : str
+        Name of the City to plot
+    """
+        
     data = data[data['City'] == city]
     pred = data.groupby('Date').agg({'prediction': 'mean'}).orderBy('Date').toPandas().set_index('Date')
     actual = data.groupby('Date').agg({pollutant: 'mean'}).orderBy('Date').toPandas().set_index('Date')
@@ -31,6 +69,8 @@ def plot_city_preds(pollutant, data, city):
     plt.figure(figsize=(14,5))
     plt.plot(pred.index, pred[f'avg(prediction)'], label='Predictions')
     plt.plot(actual.index, actual[f'avg({pollutant})'], color='C1', label='Actual', linestyle='--')
+    plt.xlabel('Year', fontsize=18)
+    plt.ylabel('AQI', fontsize=18)
     plt.legend()
 
-    plt.title(f'Predictions Vs Actual - {city, pollutant}');
+    plt.title(f'Predictions Vs Actual - {city} ({pollutant})', fontsize=20);
